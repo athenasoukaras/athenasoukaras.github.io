847b5202073dbb331c3445110f9b18a0ee4958ec
Athena <athenasoukaras@gmail.com>
2026-06-16 09:57:58 -0400

    Add files via upload

--- scripts/secret.js
+++ scripts/secret.js
@@ -1,6 +1,9 @@
+const audio = new Audio('https://codehs.com/uploads/e52e991a877acd826f14132c9950ef4c');
+
 function secretFunction() {
     var secretVariable = document.getElementById("secrettext").value
     if(secretVariable == "67"){
         document.getElementsByTagName("header")[0].setAttribute("id", "secret");
+        audio.play();
     }
 }
